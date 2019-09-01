67a645ebaa8b82677f45bafc448c4d6f8e32c6fc
JayaBalaji <jayabalaji1990@gmail.com>
2019-09-01 18:02:08 +0530

    updated data table concept

--- EntireCourse/src/test/java/Features/Login.feature
+++ EntireCourse/src/test/java/Features/Login.feature
@@ -1,13 +1,18 @@
-Feature: Application_Login
+Feature: Application_Login 
 
-Scenario Outline: Validate Successful Login
-Given User is on Login page "<URL>"
-When User enters username, password and Logs in
-Then Home page is displayed
-
-Examples:
-|URL|
-|http://www.facebook.com|
-|http://www.gmail.com|
-
-
+Scenario Outline: Validate Successful Login 
+	Given User is on Login page "<URL>" 
+	When User enters username, password and Logs in 
+	Then Home page is displayed 
+	
+	Examples: 
+		|URL|
+		|http://www.facebook.com|
+		|http://www.gmail.com|
+		
+Scenario: Validate Page load 
+	Given User is on another login page 
+		|http://www.youtube.com|
+	Then Home page is displayed 
+	
+	
--- EntireCourse/src/test/java/StepDefinition/LoginSteps.java
+++ EntireCourse/src/test/java/StepDefinition/LoginSteps.java
@@ -1,9 +1,13 @@
 package StepDefinition;
 
+import java.util.List;
+
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -19,6 +23,7 @@ public class LoginSteps {
 		FacebookPage.manage().window().maximize();
 		FacebookPage.get(url);
 		System.out.println("Opened FB1");
+		FacebookPage.close();
 	}
 
 	@When("^User enters username, password and Logs in$")
@@ -32,6 +37,25 @@ public class LoginSteps {
 		System.out.println("Print Then11");
 
 	}
+	
+	@Given("^User is on another login page$")
+	public void user_is_on_another_login_page(DataTable arg1) throws Throwable {
+	    List<List<String>> URL = arg1.raw();
+	    
+	    System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver FacebookPage = new ChromeDriver();
+		FacebookPage.manage().window().maximize();
+		FacebookPage.get(URL.get(0).get(0));
+		System.out.println("Opened via Datatable");
+		
+	}
+
+	@Then("^Home page is ok$")
+	public void home_page_is_ok() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
 	
 	
 }
