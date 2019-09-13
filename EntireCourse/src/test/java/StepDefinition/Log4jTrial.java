78d4bc3bf9d5f02fb0b4fa8545a535eef88b1564
JayaBalaji <jayabalaji1990@gmail.com>
2019-09-13 21:18:23 +0530

    added log4j

--- EntireCourse/src/test/java/StepDefinition/Log4jTrial.java
+++ EntireCourse/src/test/java/StepDefinition/Log4jTrial.java
@@ -1,6 +1,5 @@
 package StepDefinition;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 public class Log4jTrial {
@@ -10,8 +9,8 @@ public class Log4jTrial {
 		final Logger logger1 = Logger.getLogger(Log4jTrial.class);
 		//BasicConfigurator.configure();
 		
-		logger1.warn(logger1);
-		System.out.println("ONe");
+		logger1.warn("First Log Message"+logger1);
+		logger1.fatal("Object not found");
 
 	}
 
