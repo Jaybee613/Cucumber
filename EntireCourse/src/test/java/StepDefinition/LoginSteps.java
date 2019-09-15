49c09c91ba0af02d355ecbdb2ba37851bfc3cf1c
JayaBalaji <jayabalaji1990@gmail.com>
2019-09-15 12:05:20 +0530

    Smaller edits

--- EntireCourse/src/test/java/StepDefinition/LoginSteps.java
+++ EntireCourse/src/test/java/StepDefinition/LoginSteps.java
@@ -1,15 +1,6 @@
 package StepDefinition;
 
-import java.util.List;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.logging.LogLevelMapping;
-
-import Cucumber.EntireCourse.GenericFunctions;
 import Cucumber.EntireCourse.GlobalBrowser;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -19,24 +10,21 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 public class LoginSteps extends GlobalBrowser {
 
-	// private static Logger log = new Logger(LoginSteps.class.getName());
-
 	@Given("^User is on Login page \"([^\"]*)\"$")
 	public void AccessLoginPage(String url) throws Throwable {
 
-		// WebDriver FacebookPage = new ChromeDriver();
 		getDriver().manage().window().maximize();
 		getDriver().get(url);
 		System.out.println("Opened FB1");
-		//CloseDriver();
+		CloseDriver();
 
 	}
 
 	@When("^User enters username, password and Logs in$")
 	public void EnterLoginPage() throws Throwable {
-		//GenericFunctions GenericObject = new GenericFunctions();
-		//GenericObject.InitiateDriver();
+		getDriver().get("http://www.amazon.in");
 		System.out.println("Print When");
+		CloseDriver();
 
 	}
 
